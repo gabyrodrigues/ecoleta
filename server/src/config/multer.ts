8f83e8e00e44ed802bedccf4f37af1a8325f1201
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-06-15 23:09:35 -0300

    validação de tipos de imagens no upload (cadastro de pontos de coleta)

--- server/src/config/multer.ts
+++ server/src/config/multer.ts
@@ -1,10 +1,28 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+const uploadsFolder = path.resolve(__dirname, '..', '..', 'uploads');
+
+interface UploadConfig {
+    photosFolder: string;
+    uploadsFolder: string;
+  
+    storage: StorageEngine;
+  
+    limits: {
+      fileSize: number;
+    };
+  
+    fileFilter(): any;
+}
+
 export default {
+    uploadsFolder,
+    photosFolder: path.resolve(uploadsFolder, 'photos'),
+
     storage: multer.diskStorage({
-        destination: path.resolve(__dirname, '..', '..', 'uploads'),
+        destination: uploadsFolder,
         filename(request, file, callback) {
             //criando nome aleatorio para o arquivo de imagem para evitar conflitos
             const hash = crypto.randomBytes(6).toString('hex');
@@ -13,5 +31,21 @@ export default {
 
             callback(null, filename);
         }
-    })
-};
+    }),
+    limits: {
+        fileSize: 8 * 2048 * 2048
+    },
+    fileFilter: (request, file, callback) => {
+        const allowedMimes = [
+            'image/jpeg',
+            'image/jpg',
+            'image/png'
+        ];
+
+        if (allowedMimes.includes(file.mimetype)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Invalid file type. Only images are allowed."));
+        }
+    }
+} as UploadConfig;
